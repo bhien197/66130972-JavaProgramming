90d4f032062c79bfa5bb43c5dd53766db5fae599
bhien197 <hien.ntb.66cntt@ntu.edu.vn>
2026-03-11 23:10:29 +0700

    HelloWorld

---
+++ xinchao/xinchaojava.java
@@ -0,0 +1,12 @@
+package xinchao;
+
+public class xinchaojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao the gioi Java!");
+		System.out.println("Chao mung den voi lap trinh Java");
+		System.out.println("Nguyen Thai Bao Hien - 66CNTT_2");
+	}
+
+}
