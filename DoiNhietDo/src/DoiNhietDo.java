f927a646559c22870d5f4b1719ac125a34f30670
bhien197 <hien.ntb.66cntt@ntu.edu.vn>
2026-03-21 17:58:09 +0700

    DoiNhietDo
    Added functionality to convert Celsius to Kelvin.

--- DoiNhietDo/src/DoiNhietDo.java
+++ DoiNhietDo/src/DoiNhietDo.java
@@ -3,7 +3,12 @@ public class DoiNhietDo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap nhiet do C:");
+		double C = sc.nextDouble();
+		double K = C + 273.15;
+		System.out.print("Nhiet do K la:" + K);
+		sc.close();
 	}
 
 }
