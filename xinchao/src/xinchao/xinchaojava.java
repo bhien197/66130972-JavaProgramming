e924eeedd7712829306857ff5203ddd1f9fb696c
bhien197 <hien.ntb.66cntt@ntu.edu.vn>
2026-03-21 17:48:52 +0700

    Add greeting messages to xinchaojava

--- xinchao/src/xinchao/xinchaojava.java
+++ xinchao/src/xinchao/xinchaojava.java
@@ -4,7 +4,9 @@ public class xinchaojava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Xin chao the gioi Java!");
+		System.out.println("Chao mung den voi lap trinh Java");
+		System.out.println("Nguyen Thai Bao Hien - 66CNTT_2");
 	}
 
 }
