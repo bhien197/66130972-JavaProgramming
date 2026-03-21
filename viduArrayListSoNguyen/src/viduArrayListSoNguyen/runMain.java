5dc0030eb75b5cd6a3e1913c7b3eb4a0c4cf0c5a
bhien197 <hien.ntb.66cntt@ntu.edu.vn>
2026-03-21 17:55:25 +0700

    viduArrayListSoNguyen

--- viduArrayListSoNguyen/src/viduArrayListSoNguyen/runMain.java
+++ viduArrayListSoNguyen/src/viduArrayListSoNguyen/runMain.java
@@ -1,10 +1,62 @@
 package viduArrayListSoNguyen;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Khai báo biến danh sách, chứa các số nguyên
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		// a. Nhập 10 phần tử từ bàn phím
+		Scanner banPhim = new Scanner(System.in);      
+		
+		for (int i=0; i<10; i++) {
+			// Nhập 1 phần tử
+			    // In ra một hướng dẫn cho người dùng
+//			System.out.print("Mời nhập phần tử thứ " + i + ": ");
+			System.out.print("a[" + i + "]=");
+			
+			    // Cho User nhập, cất vào biến tạm
+			Integer tam = banPhim.nextInt();
+			   // Thêm vào cuối danh sách
+			dsSoNguyen.add(tam);
+		}
+		
+		
+		//b. In ra danh sách vừa nhập
+		System.out.print("DANH SÁCH VỪA NHẬP LÀ \n");
+//		for (int i=0; i<dsSoNguyen.size(); i++) {
+//			// Lấy giá trị phần tử ở vị trí i, cất vào biến tạm
+//			Integer tam = dsSoNguyen.get(i);
+//			System.out.print(tam + " ");
+//		}
+		for (Integer x: dsSoNguyen) {
+			System.out.print(x + " ");
+		}
+		 
+       //c. Đếm số phần tử chẵn
+	   int soLuongPTchan=0;
+	   for (Integer x: dsSoNguyen) {
+			if (x % 2 ==0 ) soLuongPTchan=soLuongPTchan+1;
+//			if (x % 2 ==0 ) soLuongPTchan++;
+	   }
+	   System.out.println("Số phần tử chẵn là: " + soLuongPTchan);
+	   
+//	 hoặc	
+//		for (int i=0; i<dsSoNguyen.size(); i++) {
+//		  	Integer tam = dsSoNguyen.get(i);
+//		  	if (tam % 2 ==0 ) soLuongPTchan=soLuongPTchan+1;
+//	    }
+	   
+	  //d. Tính tổng  các phần tử: Thuật toán cộng dồn
+	   int TongAll=0;
+	   for (Integer x: dsSoNguyen) {
+		   TongAll = TongAll +x;
+	   }
+	   System.out.println("Tổng tất cả các phần tử là: " + TongAll);
+	   
+		
 	}
 
 }
